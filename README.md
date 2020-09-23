8e15afdaf961b784a4c01848e816382e9971f3f4
Leigh Halliday <leighhalliday@gmail.com>
2020-09-23 07:37:55 -0400

    Managing state demo

--- README.md
+++ README.md
@@ -1,30 +1,17 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# How I Manage State in React
 
-## Getting Started
+When I'm building a React app, how do I manage state?
 
-First, run the development server:
+## App State
 
-```bash
-npm run dev
-# or
-yarn dev
-```
+Keep state as low as possible.
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+1. Local State
+2. Lift State
+3. Global State (with contexts)
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+Once you are dealing with Global State, you're welcome to use MobX, Redux, Overmind, Zustand, Recoil, etc... or just stick with `useState`.
 
-## Learn More
+## External Data
 
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/import?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+React Query, SWR, Apollo Client, urql
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,71 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState, memo, createContext, useContext } from "react";
+import { useQuery } from "react-query";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
+    <CountryProvider>
+      <HomeContent />
+    </CountryProvider>
+  );
+}
+
+const HomeContent = memo(() => {
+  return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <CountryPicker />
+      <CountryDetails />
     </div>
-  )
+  );
+});
+
+const CountryContext = createContext();
+
+function CountryProvider({ children }) {
+  const [country, setCountry] = useState("CA");
+
+  return (
+    <CountryContext.Provider value={{ country, setCountry }}>
+      {children}
+    </CountryContext.Provider>
+  );
+}
+
+async function fetchCountry(country) {
+  const response = await fetch(
+    `https://restcountries.eu/rest/v2/alpha/${country}`
+  );
+  const data = await response.json();
+  return data;
+}
+
+function CountryDetails() {
+  const { country } = useContext(CountryContext);
+  const { data, isLoading, error } = useQuery([country], fetchCountry);
+
+  if (isLoading) return <span>loading...</span>;
+  if (error) return <span>oop!! error occurred</span>;
+
+  return (
+    <div>
+      <h1>{country}</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
+
+function CountryPicker() {
+  const { country, setCountry } = useContext(CountryContext);
+
+  return (
+    <select
+      value={country}
+      onChange={(event) => {
+        setCountry(event.target.value);
+      }}
+    >
+      <option value="CA">Canada</option>
+      <option value="CO">Colombia</option>
+    </select>
+  );
 }
